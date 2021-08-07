d942d77e1afa4986be426d2d8a3d8400f5b1879a
Craig Spence <craigspence0@gmail.com>
2021-08-07 14:14:30 +1200

    initial commit

---
+++ script.js
@@ -0,0 +1,159 @@
+const LEVEL_ONE = [
+  `What do you think is the hardest part of what I do for a living?`,
+  `What reality show do you think I'm most likely to binge watch? Explain.`,
+  `What's the first thing you noticed about me?`,
+  `If Myspace was still a thing what would my profile song be?`,
+  `If you were to buy me a present, knowing nothing about me other than what I look like, what would it be?`,
+  `On a scale of 1-10, how messy do you think my car is? 1 being the cleanest 10 complete disaster, explain.`,
+  `Do you think plants thrive or die in my care? Explain.`,
+  `Do I look kind? Explain.`,
+  `Do I seem like more of a creative or analytical type? Explain.`,
+  `Do I seem like someone who would get a name tattooed on myself? Why or why not?`,
+  `Finish the sentence: Just by looking at you I'd think _____.`,
+  `What do my shoes tell you about me?`,
+  `Do you think I've ever been fired from a job? If so, what for?`,
+  `What was your first impression of me?`,
+  `What was your first impression of me on Bumble vs IRL?`,
+  `How would you describe what you think my type is in three words?`,
+  `What did you think about my first message?`,
+  `Between the two of us, who seems like the better texter? Explain.`,
+  `What does my Instagram tell you about me?`,
+  `Make an assumption about me`,
+  `What about me intrigues you?`,
+  `What do you think I'm most likely to splurge on?`,
+  `As a child, what do you think I wanted to be?`,
+  `Finish the sentence: just by looking at you I'd think ...`,
+  `Do you think I was popular in school? Explain.`,
+  `How many speeding tickets do you think I've gotten in my life?`,
+  `Do I seem like someone who would get a name tattooed on myself? Why or why not?`,
+  `Do you think I fall in love easily why or why not`,
+  `What compliment do you think I hear the most?`,
+  `Do you think I ever checked an Ex phone for evidence?`,
+  `What subject do you think I thrived at in school and did I fail any?`,
+  `Do I seem like a coffee or tea person - sweetened or unsweetened?`,
+  `As a child what do you think I wanted to be when I grow up?`,
+  `Do I seem like a morning person or a night owl?`,
+  `Do you think I was popular at school? Explain.`,
+  `Do you think I'm usually early on time or late to events. Explain.`,
+  `Do I remind you of anyone you know?`,
+  `What do you think I'm most likely to splurge on?`,
+  `How many speeding tickets you think I've gotten in my life?`,
+  `Am I someone you typically connect with?`,
+  `Do I intimidate you? Why or why not?`,
+  `What's your favorite song lyric that comes to your mind from the top of your head?`,
+  `As a child, what do you think I wanted to be?`,
+];
+
+const LEVEL_TWO = [
+  `What's been your happiest memory this past year?`,
+  `Have you changed your mind about anything recently?`,
+  `What has been your earliest recollection of happiness?`,
+  `What lesson took you the longest to un-learn?`,
+  `Are you lying to yourself about anything?`,
+  `What questions are you trying to answer most in your life right now?`,
+  `When was the last time you surprised yourself?`,
+  `What title would you give this chapter in your life?`,
+  `What do you crave more of?`,
+  `Finish the sentence: Strangers would describe me as ______ only I know that I am _______`,
+  `What's the worst pain you have ever been in that wasn't physical?`,
+  `Has a stranger ever changed your life?`,
+  `What's your first loves name and the reason you fell in love with them?`,
+  `What would your younger self not believe about your life today?`,
+  `How would you describe the feeling of being in love with one word?`,
+  `What are you more afraid of: failure or success and why?`,
+  `What is a dream you let go of?`,
+  `If you could get to know someone in your life on a deeper level who would it be and why?`,
+  `What's your mom's name and what is the most beautiful thing about her?`,
+  `What part of your life works what part of your life hurts?`,
+  `How can you become a better person?`,
+  `Do you think the image you have of yourself matches the image other people see you as?`,
+  `Are you missing anyone right now? Do you think they miss you to?`,
+  `What fast food restaurant do you think I'm most likely to drive through? What's my order?`,
+  `Do you think I intimidate others? Why or why not?`,
+  `When you are asked how are you, how often do you answer truthfully?`,
+  `How are you, really?`,
+  `What's the most unexplainable thing that happened to you?`,
+  `What's your father's name? And tell me one thing about him.`,
+  `If you could have it your way: who would you be with? Where would you be? What would you be doing?`,
+  `Describe your perfect day!`,
+  `What dating advice would you give your younger self?`,
+  `How old do you feel emotionally?`,
+  `Will I find your Ex on your Insta feed? Why or why not?`,
+  `What's the most embarrassing thing that happened to you on a date?`,
+  `What could you have done better in previous relationships?`,
+  `Is there an image of yourself you try to project on a first date you wish you could let go of?`,
+  `Finish the sentence: Dating is ____________`,
+  `What did the people who raised you teach you about love?`,
+  `Describe your perfect date.`,
+  `What has being single taught you about yourself?`,
+  `What's your least favorite question to be asked on a date? What do you wish you would be asked more?`,
+  `What are you passionate about?`,
+  `What are you not currently giving enough time to?`,
+  `What about my profile made you swipe right?`,
+];
+
+LEVEL_THREE = [
+  `Based on what you learned about me what would you recommend I read?`,
+  `What about me surprised you?`,
+  `Based on what you know about me, do you have a Netflix recommendation?`,
+  `What do you think my superpower is?`,
+  `What do you think our most important similarities are?`,
+  `What do you think is one thing I could do that would drastically improve my life?`,
+  `What would be the perfect gift for me?`,
+  `How would you describe me to a stranger?`,
+  `What do I need to hear right now?`,
+  `Based on what you've learned about me, does my social media portray me accurately?`,
+  `What is a lesson you will take away from our conversation?`,
+  `What can I help you with?`,
+  `What do you think do I fear the most?`,
+  `Where am I most qualified to give advice about?`,
+  `What about me is the hardest for you to understand?`,
+  `If we were a band what would be our name?`,
+  `Admit something.`,
+  `What parts of yourself do you see in me?`,
+  `How does one earn your vulnerability?`,
+  `Dare your partner to do something out of their comfort zone next week.`,
+  `What do you recommend I should let go of?`,
+  `What has this conversation taught you about yourself?`,
+  `What do you think my defining characteristic is?`,
+  `What question were you most afraid to answer?`,
+  `Why do you think we met?`,
+  `When this game is over, what will be something you will remember about me?`,
+  `What do you think my weakness is?`,
+  `How do our personalities compliment each other?`,
+  `What do you think I should know about myself that perhaps I'm unaware of?`,
+  `What would make you feel closer to me?`,
+  `What do you admire most about me?`,
+  `In one word, describe how you feel right now.`,
+  `Do you believe everyone has a calling? If so, do you think I've found mine?`,
+  `What do you think my defining characteristic is?`,
+  `What can we create together?`,
+  `What answer of mine made you light up?`,
+  `What's the most attractive quality about me that isn't physical?`,
+  `Am I like you expected me to be?`,
+  `What do you admire most about me?`,
+  `How would you describe me to a stranger?`,
+];
+
+const card = document.getElementById("card-content");
+
+function changeLevel(level) {
+  let questions = [];
+  if (level === 1) {
+    questions = [...LEVEL_ONE];
+  }
+  if (level === 2) {
+    questions = [...LEVEL_TWO];
+  }
+  if (level === 3) {
+    questions = [...LEVEL_THREE];
+  }
+  window.next = () => {
+    const questionN = Math.floor(Math.random() * questions.length);
+    const question = questions.splice(questionN, 1);
+    card.innerText = question;
+  };
+  next();
+}
+
+changeLevel(1);
